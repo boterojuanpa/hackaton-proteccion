3ffe7566cbb0c4d832d88aa7282f022c77b832b5
CEIBA17 <heyder.ortega@ceiba.com.co>
2017-04-01 11:36:16 -0500

    Envío de SMS

--- backend/hackaton/src/main/java/com/prueba/TurnosController.java
+++ backend/hackaton/src/main/java/com/prueba/TurnosController.java
@@ -4,7 +4,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.elibom.client.ElibomRestClient;
 
 @RestController
 public class TurnosController {
@@ -16,5 +19,11 @@ public class TurnosController {
 		System.out.println("llego "+ input.getEmail());
 		GmailEmailService emailService = new GmailEmailService();
 		emailService.sendEmail(input.getUrlTurno(), input.getEmail());
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String finalizarServicio(@RequestParam("nroCelular") String celular) {
+        ElibomRestClient elibom = new ElibomRestClient("yuber.garcia@ceiba.com.co", "YUBERg900917");
+        return elibom.sendMessage(celular, "Esto es una prueba");
 	}
 } 
