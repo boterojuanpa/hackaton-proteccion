aab7c4abc9963f193b105e4705747e8a7870b69c
CEIBA17 <heyder.ortega@ceiba.com.co>
2017-04-01 10:38:13 -0500

    Se agrega nombre a la notificación

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,8 +1,8 @@
 self.addEventListener('push', function (event) {
     console.log("Notificacion recibida");
-    console.log(`Datos: "${JSON.parse(event.data.text()).message}"`);
+    console.log(`Datos: "${event.data.text()}"`);
 
-    const title = JSON.parse(event.data).message;
+    const title = JSON.parse(event.data.text()).message;
     const options = {
         body: 'Estas 2 turnos de ser atendido',
         icon: 'assets/images/ico-push.png',
